931d42a0eb942cf72ae160435e3c0980aa1b822e
Elias Ferreira <elias.eliasferreira@gmail.com>
2019-10-08 22:18:30 -0300

    Some problems resolveds

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@
  //imports
 const express  = require('express');
 const mongoose = require('mongoose');
+const cors     = require('cors');
+const path     = require('path');
 const routes   = require('./routes');
 
 const app = express();
@@ -17,7 +19,11 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-it3b1.mongodb.net/s
     useUnifiedTopology: true
 });
 
+app.use(cors()); //Allow access from every frontend.
 app.use(express.json()); //this line turn possible tho return json format data.
+app.use('/files', express.static(
+    path.resolve(__dirname, '..', 'uploads')
+));
 app.use(routes);// call to the routes.
 
 app.listen(3333);//port to be uses by Node server.
